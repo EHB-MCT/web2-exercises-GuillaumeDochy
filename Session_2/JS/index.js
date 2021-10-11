a7d54b309081f4bab276f739310a32c0f18110a3
GuillaumeDochy <guillaume.dochy@student.ehb.be>
2021-10-11 10:41:10 +0200

    Update index.js

--- Session_2/JS/index.js
+++ Session_2/JS/index.js
@@ -1,69 +1,65 @@
-'use strict';
-
-import Team from "./team.js";
-
-let team = new Team('da charizards');
-team.describe();
+import Team from './team.js';
 
 let list = [];
-let pokemons = [];
+let pokemon = [];
 
-class Pokemons {
-    constructor() {
-        this.url = '';
-    }
-    fetcher() {
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                list = data.results;
-                list.forEach(pokemon => {
-                    fetch(`${pokemon.url}`)
-                        .then(response => {
-                            return response.json();
-                        })
-                        .then(data => {
-                            pokemons.push(data);
-                        })
-                });
-            })
-    }
+let team1 = new Team();
+
+function getData() {
+    //get the list
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        .then(response => {
+            return response.json();
+        }).then(data => {
+            list = data.results;
+            for (let element of list) {
+                fetch(element.url).then(response => {
+                    return response.json();
+                }).then(data => {
+                    pokemon.push(data);
+                })
+            }
+        });
 }
 
-let pokemon = new Pokemons();
-
-
-pokemon.fetcher();
-
 window.onload = function () {
-    setTimeout(buildList, 1000);
-    let card = document.getElementById("container");
+
+    getData();
+    setTimeout(buildList, 3000);
 
     function buildList() {
-
-        let htmlString = "";
-        let htmlStringTypes = '';
-
-        pokemons.forEach(pokemon => {
-            for (let i = 0; i < pokemon.types.length; i++) {
-                htmlStringTypes += `<p class="type" id = "${pokemon.types[i].type.name}>${pokemon.types[i].type.name}</p>`;
-            }
-            console.log(pokemon);
-            htmlString += `
-            <div class="pokemonCard" id="pokemonCard">
-            <img class="img" src="${pokemon.sprites.front_default}"></img>
-            <h3 class="name">${pokemon.name}</h3>
-            <div class="typeDiv" id="typeDiv">
-            </div>
-            <button class="teamBtn">Add to team</button>
-            </div>`;
-
+        let html = '';
+        pokemon.sort(function (a, b) {
+            return a.id - b.id;
         });
-        card.innerHTML = htmlString;
-        let type = document.getElementById("typeDiv");
-        type.innerHTML = htmlStringTypes;
+
+
+        for (let p of pokemon) {
+            html += `<div class="card" style="width: 10rem; margin: 3px;">
+            <img class="card-img-top" src="${p.sprites.front_default}" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">${p.name}</h5>
+            <a href="#" id="${p.id}" class="btn btn-primary ">Add to team</a>
+            </div>
+            </div>`
+        }
+        document.getElementById('list').innerHTML = html;
+
+        document.querySelectorAll('.btn').forEach(item => {
+            item.addEventListener('click', event => {
+                let id = event.target.id;
+            });
+        });
     }
+
+    refreshTeam();
+
+
+
+
+
+}
+
+function refreshTeam() {
+    document.getElementById('team').innerHTML = team1.describe();
 }
